1c1ea6361c7a6e4b42cf0e49ebe5411cb807f4de
hieudoanm <hieumdoan@gmail.com>
2022-11-10 21:56:06 +0700

    Rename function

--- serverless/functions/api/index.go
+++ serverless/functions/api/index.go
@@ -9,7 +9,7 @@ type HealthResponse struct {
 	Status string `json:"status"`
 }
 
-func GetHealth(writer http.ResponseWriter, request *http.Request) {
+func Handler(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
 	var healthResponse HealthResponse = HealthResponse{"healthy"}
 	json.NewEncoder(writer).Encode(healthResponse)
